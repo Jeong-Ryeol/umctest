194c1a0c4172e21371f593e495cbb899f4861d63
Jeong-Ryeol <jwr5336@naver.com>
2025-06-08 17:57:13 +0900

    10주차

---
+++ umc10week/src/main/java/umc/study/service/MemberService/MemberQueryService.java
@@ -0,0 +1,8 @@
+package umc.study.service.MemberService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import umc.study.ApiMission1.code.MemberResponseDTO;
+
+public interface MemberQueryService {
+    MemberResponseDTO.MemberInfoDTO getMemberInfo(HttpServletRequest request);
+}
---
+++ umc10week/src/main/java/umc/study/web/controller/MemberRestController.java
@@ -0,0 +1,43 @@
+package umc.study.web.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import umc.study.ApiPayload.ApiResponse;
+import umc.study.ApiMission1.code.MemberRequestDTO;
+import umc.study.ApiMission1.code.MemberResponseDTO;
+import umc.study.converter.MemberConverter;
+import umc.study.domain.Member;
+import umc.study.service.MemberService.MemberCommandService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberRestController {
+
+    private final MemberCommandService memberCommandService;
+
+    @PostMapping("/")
+    public ApiResponse<MemberResponseDTO.JoinResultDTO> join(@RequestBody @Valid MemberRequestDTO.JoinDto request){
+        Member member = memberCommandService.joinMember(request);
+        return ApiResponse.onSuccess(MemberConverter.toJoinResultDTO(member));
+    }
+
+    @PostMapping("/login")
+    @Operation(summary = "유저 로그인 API",description = "유저가 로그인하는 API입니다.")
+    public ApiResponse<MemberResponseDTO.LoginResultDTO> login(@RequestBody @Valid MemberRequestDTO.LoginRequestDTO request) {
+        return ApiResponse.onSuccess(memberCommandService.loginMember(request));
+    }
+
+    @GetMapping("/info")
+    @Operation(summary = "유저 내 정보 조회 API - 인증 필요",
+            description = "유저가 내 정보를 조회하는 API입니다.",
+            security = { @SecurityRequirement(name = "JWT TOKEN") }
+    )
+    public ApiResponse<MemberResponseDTO.MemberInfoDTO> getMyInfo(HttpServletRequest request) {
+        return ApiResponse.onSuccess(MemberQueryService.getMemberInfo(request));
+    }
+}
